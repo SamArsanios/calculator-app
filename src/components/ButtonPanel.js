d9636a646f446a23c0860befd9eca81154e69dd5
SamArsanios <samson.kibrom@hotmail.com>
2021-06-16 17:50:59 +0300

    Add Button Panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default ButtonPanel = () => {
+export default function ButtonPanel() {
   return (
     <div className="button-panel">
       <div className="buttons">
