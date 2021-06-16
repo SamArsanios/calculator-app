9c14201bddf0f90eb6b5bd1e86fc61f2c26b3d97
SamArsanios <samson.kibrom@hotmail.com>
2021-06-16 18:57:59 +0300

    Fix linter errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import './App.css';
+// import './App.css';
 
 export default function App() {
   return (
