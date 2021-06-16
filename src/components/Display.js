01ef938e994bff33c463338bf2ebf0aaa2b7707a
SamArsanios <samson.kibrom@hotmail.com>
2021-06-16 17:49:56 +0300

    Add Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-export default Display = (props) => {
-    const { result } = props
-    return (
-        <div className="display">
-            {result}
-        </div>
-    );
+export default function Display(props) {
+  const { result } = props;
+  return (
+    <div className="display">
+      {result}
+    </div>
+  );
 }
 
 Display.propTypes = {
-    result: PropTypes.string
-}
+  result: PropTypes.string,
+};
 
 Display.defaultProps = {
-    result: '0'
-}
+  result: '0',
+};
