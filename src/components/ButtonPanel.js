7331da76d7e8d733813f107402c41df032b69856
SamArsanios <samson.kibrom@hotmail.com>
2021-06-16 17:29:33 +0300

    Add panels

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 export default ButtonPanel = () => {
-    return (
-        <div className="button-panel">
-            <div className="">
-                <Button buttonName />
-                <Button buttonName />
-                <Button buttonName />
-            </div>
-        </div>
-    );
+  return (
+    <div className="button-panel">
+      <div className="buttons">
+        <Button buttonName="AC" />
+        <Button buttonName="+/-" />
+        <Button buttonName="%" />
+        <Button buttonName="÷" />
+      </div>
+      <div className="buttons">
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+      </div>
+      <div className="buttons">
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+      </div>
+      <div className="buttons">
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+        <Button buttonName />
+      </div>
+    </div>
+  );
 }
