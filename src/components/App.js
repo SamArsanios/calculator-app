88331b012a7c828a2f93b8b5215ef61e6ff42f36
SamArsanios <samson.kibrom@hotmail.com>
2021-06-16 17:49:21 +0300

    Add dispaly and buttonpanel components in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Display from './Display'
-import ButtonPanel from './ButtonPanel'
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 import './App.css';
 
-export default App = () => {
+export default function App() {
   return (
     <div className="App">
       <Display />
