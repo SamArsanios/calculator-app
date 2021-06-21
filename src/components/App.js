8a35ac3caa93e1c4e03101146d652a2cba65ce89
SamArsanios <samson.kibrom@hotmail.com>
2021-06-21 23:35:01 +0300

    Add calculate to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 export default function App() {
+  const handleClick = (button) => {
+    const result = calculate(data, button);
+    setData(result);
+  };
+
   return (
     <>
       <div className="App">
         <Display />
-        <ButtonPanel />
+        <ButtonPanel clickHandler={handleClick} />
       </div>
     </>
   );
