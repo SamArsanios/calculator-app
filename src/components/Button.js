eb7e9a5c89be853c5f3fc80c4f7897c7951a5971
SamArsanios <samson.kibrom@hotmail.com>
2021-06-21 12:14:59 +0300

    Finish style app

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Button(props) {
-  const { name, clickHandler } = props;
+  const {
+    name, color, width, clickHandler,
+  } = props;
   const handleClick = (button) => clickHandler(button);
   return (
     <button
-      className="button"
+      className={`button ${color} ${width}`}
       type="button"
       onClick={handleClick.bind(this, name)}
     >
@@ -17,9 +19,13 @@ export default function Button(props) {
 
 Button.propTypes = {
   name: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string,
   clickHandler: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
   name: '',
+  color: 'orange',
+  width: '',
 };
