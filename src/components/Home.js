2ba7970aede05a93a050b168e94f78cd3a0bba36
SamArsanios <samson.kibrom@hotmail.com>
2021-06-25 19:01:12 +0300

    Add content

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
   <div>
-    <h1 className="">Welcome to Our Page!</h1>
-    <p>
+    <h1 className="content title">Welcome to Our Page!</h1>
+    <p className="content">
       You might discover that you need to discuss the different aspects of math. Mathematics is the study of numbers,
       structures, space and change as well as the application of each. Math is more than adding and subtracting;
       math is spatial relationships, measuring and probability.
