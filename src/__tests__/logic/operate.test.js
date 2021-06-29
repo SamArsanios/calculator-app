e57fc24f9f1d79f07f4a58fa71ac89c782dc093e
SamArsanios <samson.kibrom@hotmail.com>
2021-06-29 13:59:19 +0300

    Add operate tests

---
+++ src/__tests__/logic/operate.test.js
@@ -0,0 +1,37 @@
+import Big from 'big.js';
+import operate from '../../logic/operate';
+
+describe('operate', () => {
+  it('add 2 numbers and returns a result', () => {
+    const value = operate(20, 2, '+');
+    expect(value).toStrictEqual(Big(22));
+  });
+  it('subtracts 2 numbers and returns a value', () => {
+    const value = operate(20, 2, '-');
+    expect(value).toStrictEqual(Big(18));
+  });
+  it('multiplies 2 numbers and returns a value', () => {
+    const value = operate(20, 2, '*');
+    expect(value).toStrictEqual(Big(40));
+  });
+  it('divides 2 numbers and returns a value', () => {
+    const value = operate(20, 2, '÷');
+    expect(value).toStrictEqual(Big(10));
+  });
+  it('changes a number to percentage', () => {
+    const value = operate(20, 1, '%');
+    expect(value).toStrictEqual(Big(0.2));
+  });
+  it('returns opposite number', () => {
+    const value = operate(20, -1, '+/-');
+    expect(value).toStrictEqual(Big(-20));
+  });
+  it('returns Error if invalid number is entered', () => {
+    const value = operate('somewords', 'notNumber', '+');
+    expect(value).toStrictEqual('Error');
+  });
+  it('returns Division Error when number is divided by 0', () => {
+    const value = operate(20, 0, '÷');
+    expect(value).toStrictEqual('Division Error');
+  });
+});
