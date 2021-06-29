472b715f34a7be8f44e0f408a269ad02d59c31bd
SamArsanios <samson.kibrom@hotmail.com>
2021-06-29 15:29:16 +0300

    Remove commented code

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,6 @@
 import renderer from 'react-test-renderer';
 import App from './components/App';
 
-// test('App', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Welcome to Our Page!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 describe('App', () => {
   it('renders correctly', () => {
     const tree = renderer
